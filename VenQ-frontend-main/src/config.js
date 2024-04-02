3306494d1424b1c3f850f5abe255b8bd7d4fdb9c
VenQ-Tech <161921294+VenQ-Tech@users.noreply.github.com>
2024-04-02 11:56:39 +0530

    Update config.js

--- VenQ-frontend-main/src/config.js
+++ VenQ-frontend-main/src/config.js
@@ -2,7 +2,7 @@ const config = {
   // URL: "http://localhost:4000",
   //main
   // URL :"https://venq-server.onrender.com",
-  URL :"https://venq-main-server.onrender.com",
+  URL :"https://venq-in.onrender.com/",
   // URL:"https://venqserver.onrender.com"
 };
 
